0d3c0a4b960067c65a670e11e09b05ba14e27317
Martynas Jegorovas <58346155+MJegorovas@users.noreply.github.com>
2022-12-02 15:51:51 +0200

    Failing test case

--- drivers/native-driver/src/nativeTest/kotlin/com/squareup/sqldelight/drivers/native/NativeDriverTest.kt
+++ drivers/native-driver/src/nativeTest/kotlin/com/squareup/sqldelight/drivers/native/NativeDriverTest.kt
@@ -6,12 +6,32 @@ import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import app.cash.sqldelight.driver.native.inMemoryDriver
 import co.touchlab.sqliter.DatabaseFileContext.deleteDatabase
 import com.squareup.sqldelight.driver.test.DriverTest
+import kotlin.test.Test
 
 class NativeDriverTest : DriverTest() {
   override fun setupDatabase(schema: SqlSchema): SqlDriver {
     val name = "testdb"
     deleteDatabase(name)
     return NativeSqliteDriver(schema, name)
+  }
+
+  @Test
+  fun executeAttachDatabase() {
+    driver.execute(null, "ATTACH DATABASE ? AS test", 1) {
+      bindString(0, "testdb")
+    }
+    driver.executeQuery(null, "PRAGMA database_list", { cursor ->
+      // For result debugging
+//      while (cursor.next()) {
+//        println(cursor.getString(0))
+//        println(cursor.getString(1))
+//        println(cursor.getString(2))
+//      }
+//      assert(true)
+
+      cursor.next() // This moves to row with 'main' database
+      assert(cursor.next()) // Should move to attached database row
+    }, 0)
   }
 }
 
